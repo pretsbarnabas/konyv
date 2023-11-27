3e0f72049f8eb16c77c216d1912bfde09e030e99
Préts Barnabás <prets.barnabas@gmail.com>
2023-11-27 10:07:07 +0100

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konyv
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string RealYears { get; set; }
+        public string Year { get; set; }
+        public int Country { get; set; }
+        public string Language { get; set; }
+        public string Pages { get; set; }
+        public string ImageName { get; set; }
+        public string WikipediaLink { get; set; }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace konyv
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Book> books;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ReadDatabase();
+        }
+
+        private void ReadDatabase()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Book>();
+                books = connection.Table<Book>().ToList();
+                Debug.WriteLine(books.Count);
+            }
+        }
+    }
+}
